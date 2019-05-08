314b6e6b1a1c5cb5cb335ad75cd03732be675caa
ghazalb76 <ghazal.b93@gmail.com>
2019-05-08 20:26:43 +0430

    Draw original signal and phase,length of the fft

--- HW8/5.py
+++ HW8/5.py
@@ -22,11 +22,19 @@ for i in range(-1*range_cnt, 1*range_cnt):
 y = np.fft.fft(x)
 
 phase = np.arctan(np.imag(y), np.real(y))*180/pi
+lengeth = np.sqrt(np.real(y)**2+np.imag(y)**2)
 
-
-# draw coefficinets
 x2 = np.arange(-1*range_cnt, 1*range_cnt, 1)
-plt.stem(x2, t)
+# draw coefficinets
+chart1 = plt.subplot(311)
+plt.title("Original signal")
+plt.stem(x2, x)
+chart1 = plt.subplot(312)
+plt.title("FT length")
+plt.stem(x2, lengeth)
+chart1 = plt.subplot(313)
+plt.title("FT phase")
+plt.stem(x2, phase)
 
 # # Show
 plt.show()
